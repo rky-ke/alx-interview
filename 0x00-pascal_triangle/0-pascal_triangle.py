c353a1df2eaf04ad70a9423ee2ac7e385019384d
rky-ke <ronaldyegon15@gmail.com>
2024-03-25 15:32:33 +0300

    pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,8 @@
 """
 Pascal's Triangle
 """
+
+
 def pascal_triangle(n):
     """
          returns a list of lists of
@@ -13,15 +15,9 @@ def pascal_triangle(n):
     for i in range(n):
         number_list = []
         for j in range(i+1):
-            if j==0 or j== i:
+            if j == 0 or j == i:
                 number_list.append(1)
             else:
                 number_list.append(pascal_list[i-1][j] + pascal_list[i-1][j-1])
         pascal_list.append(number_list)
-    # for i in range(n):
-    #     for j in range(n-i-1):
-    #         print(format(" ", "<2"), end="")
-    #     for j in range(i+1):
-    #         print(format(pascal_list[i][j], "<3"), end=" ")
-    #     print()
     return pascal_list
